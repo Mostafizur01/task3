73a58e22444a5a31cf27b6ca9d7f3d01701ca3c4
Mostafizur01 <mostafizurrahman43@gamil.com>
2026-06-09 14:49:48 +0600

    Task 3 complete

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ let gcd = (x, y) => {
     }
     return x;
 }
+
 let lcm = (x, y) => {
     let maltiply = x * y
     let gcdValue = gcd(x, y)
@@ -17,21 +18,29 @@ let lcm = (x, y) => {
 }
 
 app.get('/mdmostafizurrahman704_gmail_com', (req, res) => {
-    let {x, y} = req.query
+    let { x, y } = req.query
 
-    if (!x || !y) {
-        return res.send('NaN')
+    if (!x || !y || typeof x !== 'string' || typeof y !== 'string') {
+        return res.send('NaN');
     }
 
-    let numX = Number(x)
-    let numY = Number(y)
+    // 👉 এখানে পরিবর্তন! ব্র্যাকেটের পাশাপাশি ব্রাউজারের %7B এবং %7D কোডও মুছে ফেলবে
+    let cleanX = x.replace(/[{} ]|%7B|%7D/gi, '');
+    let cleanY = y.replace(/[{} ]|%7B|%7D/gi, '');
 
-    if (!Number.isInteger(numX) || !Number.isInteger(numY) || numX < 1 || numY < 1 || !/^\d+$/.test(x.trim()) || !/^\d+$/.test(y.trim())) {
-        return res.send('NaN')
+    if (!/^\d+$/.test(cleanX) || !/^\d+$/.test(cleanY)) {
+        return res.send('NaN');
     }
 
-    let result = lcm(numX, numY)
-    res.send(result.toString())
+    let numX = Number(cleanX);
+    let numY = Number(cleanY);
+
+    if (numX < 1 || numY < 1) {
+        return res.send('NaN');
+    }
+
+    let result = lcm(numX, numY);
+    res.send(result.toString());
 })
 
 app.listen(3000, () => console.log('Server run at http://localhost:3000/mdmostafizurrahman704_gmail_com'))
